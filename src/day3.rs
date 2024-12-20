8a515491db9c8de465e857360144dd5a3e45cbf3
Piotr Szyma <thompson2908@gmail.com>
2024-12-20 16:14:37 +0100

    feat: day3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,26 +1,29 @@
-use crate::file_utils::read_lines_from_file;
+use std::fs::read_to_string;
+
 use regex::Regex;
 
+// TODO: drop `pos` attribute.
 #[derive(Debug, PartialEq)]
 enum Op {
-    Enable,
-    Disable,
-    Mul { left: i64, right: i64 },
+    Enable { pos: usize },
+    Disable { pos: usize },
+    Mul { left: i64, right: i64, pos: usize },
 }
 
-fn find_op_in_line(haystack: &str) -> Vec<Op> {
+fn find_ops_in_line(haystack: &str) -> Vec<Op> {
     let re_mul =
-        Regex::new(r"mul\((?P<left_op>\d{1,3}),(?P<right_op>\d{1,3})\)|(do(n't)?\(\))").unwrap();
+        Regex::new(r"mul\((?P<left_op>\d{1,3}),(?P<right_op>\d{1,3})\)|do\(\)|don't\(\)").unwrap();
 
     let captures = re_mul.captures_iter(&haystack);
 
     let result: Vec<_> = captures
         .map(|c| {
-            let full_match = c.get(0);
+            let full_match = c.get(0).unwrap();
+            let start_pos = full_match.start();
 
-            match full_match.unwrap().as_str() {
-                "don't()" => Op::Disable,
-                "do()" => Op::Enable,
+            match full_match.as_str() {
+                "don't()" => Op::Disable { pos: start_pos },
+                "do()" => Op::Enable { pos: start_pos },
                 _ => {
                     let left = c
                         .name("left_op")
@@ -30,7 +33,11 @@ fn find_op_in_line(haystack: &str) -> Vec<Op> {
                         .name("right_op")
                         .map_or(0, |m| m.as_str().parse::<i64>().unwrap());
 
-                    Op::Mul { left, right }
+                    Op::Mul {
+                        left,
+                        right,
+                        pos: start_pos,
+                    }
                 }
             }
         })
@@ -40,48 +47,49 @@ fn find_op_in_line(haystack: &str) -> Vec<Op> {
 }
 
 fn task1_run(input_path: &str) -> Result<i64, Box<dyn std::error::Error>> {
-    let result = read_lines_from_file(input_path)
-        .map(|l| {
-            find_op_in_line(&l.unwrap())
-                .iter()
-                .map(|v| match v {
-                    Op::Mul { left, right } => left * right,
-                    _ => 0,
-                })
-                .sum::<i64>()
+    let data = read_to_string(input_path).unwrap();
+    let result = find_ops_in_line(&data)
+        .iter()
+        .map(|v| match v {
+            Op::Mul {
+                left,
+                right,
+                pos: _,
+            } => left * right,
+            _ => 0,
         })
-        .sum();
+        .sum::<i64>();
     Ok(result)
 }
 
 fn task2_run(input_path: &str) -> Result<i64, Box<dyn std::error::Error>> {
-    let result = read_lines_from_file(input_path)
-        .map(|l| {
-            let ops = find_op_in_line(&l.unwrap());
+    let data = read_to_string(input_path).unwrap();
+    let ops = find_ops_in_line(&data);
 
-            let mut enabled = true;
-            let mut total = 0;
+    let mut enabled = true;
+    let mut total = 0;
 
-            for op in ops {
-                match op {
-                    Op::Disable => {
-                        enabled = false;
-                    }
-                    Op::Enable => {
-                        enabled = true;
-                    }
-                    Op::Mul { left, right } => {
-                        if enabled {
-                            total += left * right
-                        }
-                    }
+    for op in ops {
+        match op {
+            Op::Disable { pos: _ } => {
+                enabled = false;
+            }
+            Op::Enable { pos: _ } => {
+                enabled = true;
+            }
+            Op::Mul {
+                left,
+                right,
+                pos: _,
+            } => {
+                if enabled {
+                    total += left * right
                 }
             }
+        }
+    }
 
-            total
-        })
-        .sum();
-    Ok(result)
+    Ok(total)
 }
 
 pub fn task1() -> Result<i64, Box<dyn std::error::Error>> {
@@ -100,42 +108,69 @@ mod tests {
     fn find_op_in_line_mul_op_test() {
         assert_eq!(
             vec![
-                Op::Mul { left: 1, right: 2 },
+                Op::Mul {
+                    left: 1,
+                    right: 2,
+                    pos: 0,
+                },
                 Op::Mul {
                     left: 123,
                     right: 456,
+                    pos: 8,
                 },
             ],
-            find_op_in_line("mul(1,2)mul(123,456)")
+            find_ops_in_line("mul(1,2)mul(123,456)")
         );
 
         assert_eq!(
             Vec::<Op>::new(),
-            find_op_in_line("mul(4*, mul(6,9!, ?(12,34)")
+            find_ops_in_line("mul(4*, mul(6,9!, ?(12,34)")
         );
 
-        assert_eq!(Vec::<Op>::new(), find_op_in_line("mul ( 2 , 4 )"));
+        assert_eq!(Vec::<Op>::new(), find_ops_in_line("mul ( 2 , 4 )"));
         assert_eq!(
             vec![
-                Op::Mul { left: 2, right: 4 },
-                Op::Mul { left: 5, right: 5 },
-                Op::Mul { left: 11, right: 8 },
-                Op::Mul { left: 8, right: 5 },
+                Op::Mul {
+                    left: 2,
+                    right: 4,
+                    pos: 1
+                },
+                Op::Mul {
+                    left: 5,
+                    right: 5,
+                    pos: 29
+                },
+                Op::Mul {
+                    left: 11,
+                    right: 8,
+                    pos: 53
+                },
+                Op::Mul {
+                    left: 8,
+                    right: 5,
+                    pos: 62
+                },
             ],
-            find_op_in_line(
+            find_ops_in_line(
                 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
             )
         );
     }
 
     #[test]
-    fn find_mul_in_line_disable_op_test() {
-        assert_eq!(Vec::<Op>::new(), find_op_in_line("don't"));
-        assert_eq!(Vec::<Op>::new(), find_op_in_line("dodon'tdo"));
-        assert_eq!(vec![Op::Disable], find_op_in_line("don't()"));
+    fn find_op_in_line_disable_enable_test() {
+        assert_eq!(Vec::<Op>::new(), find_ops_in_line("don't"));
+        assert_eq!(Vec::<Op>::new(), find_ops_in_line("do"));
+        assert_eq!(Vec::<Op>::new(), find_ops_in_line("dodon'tdo"));
+        assert_eq!(vec![Op::Disable { pos: 0 }], find_ops_in_line("don't()"));
+        assert_eq!(vec![Op::Enable { pos: 0 }], find_ops_in_line("do()"));
         assert_eq!(
-            vec![Op::Disable, Op::Enable, Op::Disable],
-            find_op_in_line("don't()do()don'tdon't()")
+            vec![
+                Op::Disable { pos: 0 },
+                Op::Enable { pos: 7 },
+                Op::Disable { pos: 16 }
+            ],
+            find_ops_in_line("don't()do()don'tdon't()")
         );
     }
 
@@ -156,7 +191,6 @@ mod tests {
 
     #[test]
     fn task2() {
-        // too high
-        assert_eq!(91634027, task2_run("data/day3.txt").unwrap())
+        assert_eq!(89349241, task2_run("data/day3.txt").unwrap());
     }
 }
